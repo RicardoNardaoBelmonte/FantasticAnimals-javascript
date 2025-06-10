ba64f2986b706d8b7ecd2de637da1ffe3e59db6f
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-06-10 13:08:27 -0300

    setting script.js to imports and initialize the modules

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import initAcordionList from './module/initAcordionList.js';
+initAcordionList();
+
+import initScrollSuave from './module/initScrollSuave.js';
+initScrollSuave();
+
+import initTabNav from './module/initTabNav.js';
+initTabNav();
+
+import initModal from './module/modal.js';
+initModal();
+
+import dropDownMenu from './module/dropdownMenu.js';
+dropDownMenu();
